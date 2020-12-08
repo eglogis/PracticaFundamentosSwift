7f4942925d32204cfb807afc88704db5995b3c76
Samuel Bautista <samu6333@gmail.com>
2020-12-08 14:09:37 +0100

    Add Splash view in Main.storyboard

---
+++ Pods/Target Support Files/Pods-PracticaFundamentosiOS/Pods-PracticaFundamentosiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PracticaFundamentosiOS : NSObject
+@end
+@implementation PodsDummy_Pods_PracticaFundamentosiOS
+@end
--- PracticaFundamentosiOS/Controllers/ViewController.swift
+++ PracticaFundamentosiOS/Controllers/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets de las vista
+    @IBOutlet var loading: UIActivityIndicatorView?
+    
+    
+    // MARK: - Metodos del Ciclo de vida
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loading?.startAnimating()
+        navigateToHomeView()
     }
-
-
+    
+    // MARK: - Metodos privados
+    private func navigateToHomeView() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) { [weak self] in
+            self?.loading?.stopAnimating()
+        }
+    }
 }
 
