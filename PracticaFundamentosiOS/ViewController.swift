7446cfc3ef143a61a82408075057dda371c72ad2
Samuel Bautista <samu6333@gmail.com>
2020-12-08 12:02:03 +0100

    Initial Commit

---
+++ PracticaFundamentosiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaFundamentosiOS
+//
+//  Created by Samuel Bautista Sánchez on 8/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
